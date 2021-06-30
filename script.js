3baa8959442f14e5854bc6672ece730e2c9242dd
Harin23 <63739311+Harin23@users.noreply.github.com>
2021-06-29 21:35:13 -0600

    progress bar 'in progress'

---
+++ script.js
@@ -0,0 +1,99 @@
+$(document).ready(()=>{
+    const canvas = document.getElementById('mycanvas');
+    const w = window.innerWidth-3;
+    const h = window.innerHeight/2;
+
+    wr = w*0.1;
+    hr = h*0.2;
+    xr = (w*0.5) - (0.5*wr);
+    yr = (h*0.5) - (0.5*hr);
+  
+    r = hr*0.5;
+    yc = yr + (0.5*hr);
+    xc1 = xr+1;
+    xc2 = xr + wr - 1;
+
+    canvas.width = w;
+    canvas.height = h;
+
+    const ctx = canvas.getContext("2d");
+
+    grayBar=()=>{
+        ctx.fillStyle = "gray"
+        ctx.beginPath();
+        ctx.fillRect(xr,yr,wr,hr);
+    
+        ctx.fillStyle = "gray"
+        ctx.beginPath();
+        ctx.arc(xc1,yc,r,0.5*Math.PI,1.5*Math.PI);
+        ctx.fill();
+        
+        ctx.beginPath();
+        ctx.arc(xc2,yc,r,1.5*Math.PI,0.5*Math.PI);
+        ctx.fill();
+    }
+
+    grayBar();
+
+    $("#reset").click(grayBar);
+
+    var err_triggered = false;
+    $("#go").click(()=>{
+        var input = document.getElementById('percentage').value;
+        document.getElementById('percentage').value = "";
+        const err = document.getElementById('err');
+        input = parseFloat(input);
+        if(isNaN(input) === true || typeof(input) !== "number"){
+            err.classList.remove('error');
+            err_triggered = true;
+        }else{
+            if(err_triggered === true){
+                err.classList.add('error');
+                err_triggered = false;
+            }
+            input = Math.round(input*100)/10000;
+
+            if(input > 1){
+                input = 1;
+            }else if(input < 0){
+                input = 0;
+            };
+            var barLength = wr + (r*2);
+            var fillLength = barLength * input;
+            if(fillLength <= r){
+                var theta = Math.acos((r-fillLength)/r);
+                var alpha = Math.PI - theta;
+                var phi = 2 * theta;
+                ctx.fillStyle = "red";
+                ctx.beginPath();
+                ctx.arc(xc1,yc,r,alpha, alpha+phi);
+                ctx.fill();
+            }else if(fillLength <= r + wr ){
+                ctx.fillStyle = "red"
+                ctx.beginPath();
+                ctx.arc(xc1,yc,r,0.5*Math.PI,1.5*Math.PI);
+                ctx.fill();
+                
+                ctx.fillStyle = "red"
+                ctx.beginPath();
+                ctx.fillRect(xr,yr,fillLength-r,hr);
+                
+            }else{
+                ctx.fillStyle = "red"
+                ctx.beginPath();
+                ctx.fillRect(xr,yr,wr,hr);
+
+                ctx.fillStyle = "red"
+                ctx.beginPath();
+                ctx.arc(xc2,yc,fillLength-r-wr,1.5*Math.PI,0.5*Math.PI);
+                ctx.fill();
+            
+                ctx.fillStyle = "red"
+                ctx.beginPath();
+                ctx.arc(xc1,yc,r,0.5*Math.PI,1.5*Math.PI);
+                ctx.fill();
+            }
+            
+        }
+    })
+})
